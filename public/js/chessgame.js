35fe9cfa6ada1d12c0d123499e5c71151d13c41d
SamarthKasar123 <samarthkasar9924@gmail.com>
2024-12-26 18:16:57 +0530

    Day -1 Fronend Code Setup Done

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,2 +1,22 @@
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
 
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row,rowindex)=>{
+        row.forEach((square,squareindex)=>{
+            
+        });
+    });
+};
+
+const handleMove = () => {};
+
+const getPieceUnicode = () => {};
