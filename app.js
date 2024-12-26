6a6918b0cf998def5f03947e87592775eaeac8b0
SamarthKasar123 <samarthkasar9924@gmail.com>
2024-12-26 17:48:40 +0530

    Backend Setup Completed

--- app.js
+++ app.js
@@ -24,6 +24,49 @@ app.get("/",(req,res)=>{
 
 io.on("connection",function (uniquesocket) {
     console.log("connected");
+
+    if (!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole","w");
+    }
+    else if (!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole","b");
+    }
+    else {
+        uniquesocket.emit("spectatorRole");
+    }
+
+    uniquesocket.on("disconnect",function () {
+
+        if (uniquesocket.id === players.white) {
+            delete players.white;
+        }
+        else if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    });
+
+    uniquesocket.on("move",(move)=>{
+        try {
+            if (chess.turn() === "w" && uniquesocket.id !== players.white) return;
+            if (chess.turn() === "b" && uniquesocket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if (result) {
+                currentPlayer = chess.turn();
+                io.emit("move",move)
+                io.emit("boardState",chess.fen());
+            }
+            else {
+                console.log("Invalid Move",move);
+                uniquesocket.emit("Invalid Move",move);
+            }
+        } catch(err) {
+            console.log(err);
+            uniquesocket.emit("Invalid Move:",move);
+        }
+    });
 });
 
 server.listen(3000,function() {
